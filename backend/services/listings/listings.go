9073c3fc4ea97ae489b3fdbdacc242c3c014214d
jalexanderII <a.joeljr@hotmail.com>
2021-12-18 17:48:52 -0500

    add grpc_web frontend template

--- backend/services/listings/listings.go
+++ backend/services/listings/listings.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"net/http"
 
 	"github.com/hashicorp/go-hclog"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	config "github.com/jalexanderII/zero_microservice"
 	listingsDB "github.com/jalexanderII/zero_microservice/backend/services/listings/database"
 	"github.com/jalexanderII/zero_microservice/backend/services/listings/server"
 	contentStore "github.com/jalexanderII/zero_microservice/backend/services/listings/store"
 	"github.com/jalexanderII/zero_microservice/gen/listings"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -36,7 +41,28 @@ func main() {
 	reflection.Register(grpcServer)
 
 	l.Info("Server started", "port", lis.Addr().String())
-	if err := grpcServer.Serve(lis); err != nil {
-		l.Error("Error starting server", "error", err)
+	go func() {
+		log.Fatal("Serving gRPC: ", grpcServer.Serve(lis).Error())
+	}()
+
+	// From https://rogchap.com/2019/07/26/in-process-grpc-web-proxy/
+	grpcWebServer := grpcweb.WrapServer(grpcServer)
+	httpServer := &http.Server{
+		Addr: config.WEBPROXYPORT,
+		Handler: h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.ProtoMajor == 2 {
+				grpcWebServer.ServeHTTP(w, r)
+			} else {
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+				w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+				w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-User-Agent, X-Grpc-Web")
+				w.Header().Set("grpc-status", "")
+				w.Header().Set("grpc-message", "")
+				if grpcWebServer.IsGrpcWebRequest(r) {
+					grpcWebServer.ServeHTTP(w, r)
+				}
+			}
+		}), &http2.Server{}),
 	}
+	log.Fatal("Serving Proxy: ", httpServer.ListenAndServe().Error())
 }
---
+++ frontend/zero/proto/listings_pb.js
@@ -0,0 +1,25 @@
+// source: listings.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+var jspb = require('google-protobuf');
+var goog = jspb;
+var global = Function('return this')();
+
+var apartment_pb = require('./apartment_pb.js');
+goog.object.extend(proto, apartment_pb);
+var building_pb = require('./building_pb.js');
+goog.object.extend(proto, building_pb);
+var realtor_pb = require('./realtor_pb.js');
+goog.object.extend(proto, realtor_pb);
+var content_pb = require('./content_pb.js');
+goog.object.extend(proto, content_pb);
