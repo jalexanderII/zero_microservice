6d72b066aba04628e57eb061fccdeae7bfb3c377
jalexanderII <a.joeljr@hotmail.com>
2021-12-21 20:49:47 -0500

    call backend grpc from frontend

--- frontend/zero/src/App.tsx
+++ frontend/zero/src/App.tsx
@@ -1,9 +1,10 @@
+import * as grpcWeb from 'grpc-web';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 // Import the client and the message definition
 import { ListingsClient } from './proto/ListingsServiceClientPb';
-import { GetApartmentRequest } from './proto/apartment_pb';
+import { GetApartmentRequest, Apartment } from './proto/apartment_pb';
 
 function App() {
   var echoService = new ListingsClient('http://localhost:8080');
@@ -11,15 +12,22 @@ function App() {
   var request = new GetApartmentRequest();
   request.setId(1);
   var metadata = {};
-  echoService.getApartment(request, metadata, function (err, response) {
-    if (err) {
-      console.log(err.code);
-      console.log(err.message);
-    } else {
-      console.log(response.toObject());
+
+  const call = echoService.getApartment(request, metadata,
+    (err: grpcWeb.RpcError, response: Apartment) => {
+      if (err) {
+        console.log(err.code);
+        console.log(err.message);
+      } else {
+        console.log(response.toObject());
+      }
+    });
+  call.on('status', (status: grpcWeb.Status) => {
+    if (status.metadata) {
+      console.log('Received metadata');
+      console.log(status.metadata);
     }
   });
-
   
   return (
     <div className="App">
